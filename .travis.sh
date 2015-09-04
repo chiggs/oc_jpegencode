bc7403dac4e43b28e93bf57d736ebcf66b98b510
Chiggs <chris.higgs@potentialventures.com>
2015-09-04 11:20:14 +0100

    Attempt a build matrix of synthesis and simulation

---
+++ .travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+# Synthesis build
+if [ "${SYNTHESIS}" = "true" ]; then
+    wget https://www.dropbox.com/s/xvum1md2deppgre/quartus_14.0.tar.gz?dl=0
+    mv quartus* quartus_14.0.tar.gz
+    tar -zxf quartus_14.0.tar.gz
+    cd syn && PATH=$PWD/../altera/14.0/quartus/bin:$PATH make &
+    pid=$!
+
+    # Often no output so have to force output
+    while kill -0 $pid >/dev/null 2>&1; do sleep 5m; echo Build still running, honest; done
+fi
+
+
+# Simulation build
+if [ "SIMULATION}" = "true" ]; then
+    git clone https://github.com/potentialventures/cocotb.git
+    export COCOTB=$PWD/cocotb
+    cd tb && make
+fi
